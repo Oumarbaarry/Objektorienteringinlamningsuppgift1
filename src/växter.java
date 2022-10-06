0aa180daee02fa1dde33c246feef9d24a6a1e63c
Oumarbaarry <115113404+Oumarbaarry@users.noreply.github.com>
2022-10-06 12:45:54 +0000

    Update växter.java

--- src/växter.java
+++ src/växter.java
@@ -1,6 +1,6 @@
 public abstract class växter implements VäxterInterface{
 
-    //Här ger jag ut variablar som är nödvändiga för inläsning av växt
+    
     private double längd;
     private String name;
 
@@ -9,12 +9,12 @@ public abstract class växter implements VäxterInterface{
     String typFöda;
 
 
-    //Skapar en konstruktor
+    
     public växter (double längd, String name){
         this.längd=längd;
         this.name=name;
 
-        // Sedan använder jag getters för att kunna kalla på de olika variablarna
+        
     }
     public double getLängd(){
         return längd;
